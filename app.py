8b0ff3644c3ddbd29bb12f494a8967b834767d3b
Matt <91123443+vordkan@users.noreply.github.com>
2024-03-05 20:00:21 +0100

    aggiunta colori

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import mysql.connector
 
 from flask import Flask, request, render_template, redirect, url_for, jsonify, flash
 from datetime import datetime
+import random
 
 app = Flask(__name__,  static_folder='static')
 
@@ -83,6 +84,11 @@ def submit():
         print(f"Si è verificato un errore durante il salvataggio della prenotazione nel database: {e}")
         return jsonify({'success': False, 'error': str(e)})
 
+import json
+
+def load_employee_colors():
+    with open('employee_colors.json') as f:
+        return json.load(f)
 
 @app.route('/get_daily_schedule')
 def get_daily_schedule():
@@ -92,6 +98,12 @@ def get_daily_schedule():
 
     cursor.execute("SELECT id, HOUR(data_inizio), HOUR(data_fine), dipendente, nome, descrizione FROM prenotazione WHERE DATE(data_inizio) = %s AND sede = 'Frattamaggiore'", (current_date,))
     prenotazioni = cursor.fetchall()
+
+    # Ottieni la lista dei dipendenti
+    employees = leggi_dipendenti()
+
+    # Leggi i colori per i dipendenti dal file JSON
+    employee_colors = load_employee_colors()
 
     for prenotazione in prenotazioni:
         prenotazione_id = prenotazione[0]  # ID della prenotazione
@@ -113,24 +125,31 @@ def get_daily_schedule():
             daily_schedule[ora_str][dipendente].append({'id': prenotazione_id, 'cliente': cliente.lower(), 'descrizione': descrizione.lower()})
 
     html_table = '<table border="1"><tr><th>Ora</th>'
-    for dipendente in leggi_dipendenti():
-        html_table += f'<th>{dipendente}</th>'
+    for dipendente in employees:
+        # Usa i colori caricati dal file JSON per i dipendenti
+        employee_color = employee_colors.get(dipendente, '#FFFFFF')  # Default to white if color not found
+        html_table += f'<th style="background-color: {employee_color}">{dipendente}</th>'
     html_table += '</tr>'
 
     for ora in range(7, 23):
         ora_str = str(ora).zfill(2)
         html_table += f'<tr><td>{ora_str}:00</td>'
-        for dipendente in leggi_dipendenti():
+        for dipendente in employees:
             prenotazioni = daily_schedule.get(ora_str, {}).get(dipendente, [])
             cell_content = ''
             for prenotazione in prenotazioni:
                 prenotazione_id = prenotazione['id']
                 cliente = prenotazione['cliente']
                 descrizione = prenotazione['descrizione']
+                # Usa i colori caricati dal file JSON per le celle delle prenotazioni
+                employee_color = employee_colors.get(dipendente, '#FFFFFF')  # Default to white if color not found
+                cell_content += f'<div style="background-color: {employee_color};">'
                 cell_content += f'{cliente}<br><span style="font-size: 12px;">{descrizione}</span><br>'
                 cell_content += f'<span style="cursor: pointer;" onclick="eliminaPrenotazione({prenotazione_id}, \'{cliente}\', \'{descrizione}\')"> 🗑 </span>'
                 cell_content += f'<span style="cursor: pointer;" onclick="modificaPrenotazione({prenotazione_id})"> 📝 </span>'
                 cell_content += f'<span style="cursor: pointer;" onclick="modificaPrenotazione_ora({prenotazione_id})"> 🕒 </span><br>'
+                cell_content += '</div>'
+
             html_table += f'<td>{cell_content}</td>'
         html_table += '</tr>'
 
@@ -146,6 +165,12 @@ def get_daily_schedule_aversa():
     cursor.execute("SELECT id, HOUR(data_inizio), HOUR(data_fine), dipendente, nome, descrizione FROM prenotazione WHERE DATE(data_inizio) = %s AND sede = 'Aversa'", (current_date,))
     prenotazioni = cursor.fetchall()
 
+    # Ottieni la lista dei dipendenti
+    employees = leggi_dipendenti_aversa()
+
+    # Leggi i colori per i dipendenti dal file JSON
+    employee_colors = load_employee_colors()
+
     for prenotazione in prenotazioni:
         prenotazione_id = prenotazione[0]  # ID della prenotazione
         ora_inizio = prenotazione[1]
@@ -166,34 +191,36 @@ def get_daily_schedule_aversa():
             daily_schedule[ora_str][dipendente].append({'id': prenotazione_id, 'cliente': cliente.lower(), 'descrizione': descrizione.lower()})
 
     html_table = '<table border="1"><tr><th>Ora</th>'
-    for dipendente in leggi_dipendenti_aversa():
-        html_table += f'<th>{dipendente}</th>'
+    for dipendente in employees:
+        # Usa i colori caricati dal file JSON per i dipendenti
+        employee_color = employee_colors.get(dipendente, '#FFFFFF')  # Default to white if color not found
+        html_table += f'<th style="background-color: {employee_color}">{dipendente}</th>'
     html_table += '</tr>'
 
     for ora in range(7, 23):
         ora_str = str(ora).zfill(2)
         html_table += f'<tr><td>{ora_str}:00</td>'
-        for dipendente in leggi_dipendenti_aversa():
+        for dipendente in employees:
             prenotazioni = daily_schedule.get(ora_str, {}).get(dipendente, [])
             cell_content = ''
             for prenotazione in prenotazioni:
                 prenotazione_id = prenotazione['id']
                 cliente = prenotazione['cliente']
                 descrizione = prenotazione['descrizione']
+                # Usa i colori caricati dal file JSON per le celle delle prenotazioni
+                employee_color = employee_colors.get(dipendente, '#FFFFFF')  # Default to white if color not found
+                cell_content += f'<div style="background-color: {employee_color};">'
                 cell_content += f'{cliente}<br><span style="font-size: 12px;">{descrizione}</span><br>'
                 cell_content += f'<span style="cursor: pointer;" onclick="eliminaPrenotazione({prenotazione_id}, \'{cliente}\', \'{descrizione}\')"> 🗑 </span>'
                 cell_content += f'<span style="cursor: pointer;" onclick="modificaPrenotazione({prenotazione_id})"> 📝 </span>'
                 cell_content += f'<span style="cursor: pointer;" onclick="modificaPrenotazione_ora({prenotazione_id})"> 🕒 </span><br>'
+                cell_content += '</div>'
 
             html_table += f'<td>{cell_content}</td>'
         html_table += '</tr>'
 
     return html_table
 
-from flask import jsonify
-from datetime import datetime
-
-from datetime import datetime
 
 @app.route('/get_weekly_schedule', methods=['POST'])
 def get_weekly_schedule():
@@ -375,21 +402,73 @@ def leggi_dipendenti_tot():
 def get_dipendenti():
     return jsonify(leggi_dipendenti_tot())
 
+import re
+
+def add_employee_to_json(nome, colore_esadecimale):
+    try:
+        with open('employee_colors.json', 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        # Se il file non esiste, crealo con un dizionario vuoto
+        data = {}
+
+    try:
+        # Verifica se il colore esadecimale è nel formato corretto (#RRGGBB)
+        if not re.match(r'^#[0-9a-fA-F]{6}$', colore_esadecimale):
+            raise ValueError("Il colore esadecimale deve essere nel formato '#RRGGBB'")
+
+        # Aggiungi il colore al dizionario
+        data[nome] = colore_esadecimale
+
+        # Scrivi il dizionario aggiornato nel file JSON
+        with open('employee_colors.json', 'w') as f:
+            json.dump(data, f, indent=4)
+    except Exception as e:
+        print(f"Si è verificato un errore durante l'aggiunta del dipendente al file JSON: {e}")
+
+def generate_pastel_color():
+    # Genera un colore pastello casuale
+    r = int(random.uniform(0, 256))
+    g = int(random.uniform(0, 256))
+    b = int(random.uniform(0, 256))
+    # Mischiare i colori per renderli pastello
+    r = (r + 255) // 2
+    g = (g + 255) // 2
+    b = (b + 255) // 2
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
 
 @app.route('/aggiungi_dipendente', methods=['POST'])
 def aggiungi_dipendente():
     nome = request.form['nome']
     sede = request.form['sede']
 
+    # Genera un colore pastello casuale
+    colore_pastello = generate_pastel_color()
+
     try:
         cursor.execute("INSERT INTO dipendente (nome, sede) VALUES (%s, %s)", (nome, sede))
         conn.commit()
         print("Dipendente aggiunto con successo!")
+
+        # Aggiungi il dipendente al file JSON con un colore pastello casuale
+        add_employee_to_json(nome, colore_pastello)
+
         return redirect(url_for('index'))
     except Exception as e:
         print(f"Si è verificato un errore durante l'aggiunta del dipendente nel database: {e}")
         return "Si è verificato un errore durante l'aggiunta del dipendente."
 
+
+def remove_employee_from_json(nome):
+    try:
+        with open('employee_colors.json', 'r') as f:
+            data = json.load(f)
+            if nome in data:
+                del data[nome]  # Rimuovi il dipendente dal dizionario JSON
+                with open('employee_colors.json', 'w') as f:
+                    json.dump(data, f, indent=4)  # Scrivi il dizionario aggiornato nel file JSON
+    except Exception as e:
+        print(f"Si è verificato un errore durante la rimozione del dipendente dal file JSON: {e}")
 
 @app.route('/elimina_dipendente', methods=['POST'])
 def elimina_dipendente():
@@ -399,6 +478,10 @@ def elimina_dipendente():
         cursor.execute("DELETE FROM dipendente WHERE nome = %s", (dipendente,))
         conn.commit()
         print("Dipendente eliminato con successo!")
+
+        # Rimuovi il dipendente anche dal file JSON
+        remove_employee_from_json(dipendente)
+
         return redirect(url_for('index'))
     except Exception as e:
         print(f"Si è verificato un errore durante l'eliminazione del dipendente nel database: {e}")
@@ -530,7 +613,5 @@ def elimina_merce():
         return jsonify({'success': False, 'error': str(e)})
 
 #---------------------------------------------------------------------------------------------------------------------#
-
-
 if __name__ == "__main__":
     app.run(debug=True)
