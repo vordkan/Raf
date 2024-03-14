7c9e1b4a8be0767a4c8ba04c7c3e3222dad5b71c
Matt <91123443+vordkan@users.noreply.github.com>
2024-03-14 21:54:44 +0100

    eliminato file inutili

--- avvio.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Set WshShell = CreateObject("WScript.Shell") > hide.vbs
-echo WshShell.Run "python app.py", 0 >> hide.vbs
-echo WScript.Sleep 1000 >> hide.vbs
-echo WshShell.Run "http://127.0.0.1:5000/", 1 >> hide.vbs
-cscript //nologo hide.vbs
-del hide.vbs
